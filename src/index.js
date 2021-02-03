8b3d117370ef58c539c7c4877c183eae38c5dcac
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2021-02-03 14:38:00 +0530

    React Base Setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import "primereact/resources/themes/md-light-deeppurple/theme.css";
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
-import "primeflex/primeflex.css";
-import React from "react";
-import ReactDOM from "react-dom";
-import "./assets/main.css";
-import "./assets/custom.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
