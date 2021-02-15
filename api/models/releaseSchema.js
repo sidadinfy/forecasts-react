706bbed7ee146cba329d665706d446e745512911
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2021-02-15 16:54:47 +0530

    Added Releases Schema

---
+++ api/models/releaseSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const releaseSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  product_category: { type: String, required: true },
+  sku_code: { type: String, required: true, index: { unique: true } },
+  uom: { type: String, required: true },
+  lead_time: { type: String, required: true },
+  sourcing_location: { type: String, required: true },
+  days_of_stock: { type: Number, required: true, default: 0 },
+  intransit_inventory_days: { type: Number, required: true, default: 0 },
+  forecast_demand: { type: Number, required: true, default: 0 },
+  recommended_order: { type: Number, required: true, default: 0 },
+  revised_order_qty: { type: Number, default: 0 },
+});
+
+module.exports = mongoose.model("Release", releaseSchema);
